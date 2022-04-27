6769d26cd23071582817f52be8f66180d11a9cac
Thomas Tang <hello@tangkunyin.com>
2022-04-27 10:11:47 +0800

    update nodeDeps global install way
    Signed-off-by: Thomas Tang <thomas@tangkunyin.com>

--- bin/inode_global.sh
+++ bin/inode_global.sh
@@ -1,50 +1,65 @@
 #!/bin/bash
 
+corepack enable
+
+# ------- need to be installed globally ------
+globalDeps=(
+    # Dev tools
+    cloc
+    nrm
+    pm2
+    npm-check-updates
+    npm-check
+    serve
+    json-server
+    concurrently
+    verdaccio
+    lerna
+    commitizen
+    gitmoji-cli
+    git-cz
+    # Lang & Framework-CLIs
+    typescript
+    hexo
+    vuepress
+    vitepress
+    @nestjs/cli
+    @vue/cli
+    @tarojs/cli
+    @warriorjs/cli
+    create-react-app
+    react-devtools
+    @react-native-community/cli
+    create-react-native-app
+    create-react-native-module
+    # lint && babel plugins
+    babel-eslint
+    eslint
+    tslint
+    lint-staged
+    eslint-plugin-html
+    eslint-config-standard
+    eslint-plugin-node
+    eslint-plugin-promise
+    eslint-plugin-standard
+)
+
+function yarnGlobalAdd() {
+    echo "======= 本次通过 yarn global add 共计需要安装依赖 ${#globalDeps[*]} 个，请耐心等待 ======="
+    for dependency in ${globalDeps[*]} 
+    do
+        yarn global add $dependency --prefix /usr/local
+    done
+}
+
+
 ## Base NPM Tools
-npm install -g pnpm
 npm install -g yarn
+npm install -g pnpm
 npm install -g node-gyp
 
-# Usefull Dev tools
-yarn global add cloc
-yarn global add nrm
-yarn global add pm2
-yarn global add npm-check-updates
-yarn global add npm-check # 一样的功能，不过表情比较多
-yarn global add serve # 静态资源服务器
-yarn global add json-server
-yarn global add concurrently
-yarn global add verdaccio #npm proxy
-yarn global add lerna
-yarn global add commitizen
-yarn global add gitmoji-cli
-yarn global add git-cz
 
-# Lang & Framework-CLIs
-yarn global add typescript
-yarn global add hexo
-yarn global add vuepress
-yarn global add vitepress
+## use yarn2
+#yarn set version stable
 
-yarn global add @nestjs/cli
-
-yarn global add @vue/cli
-yarn global add @tarojs/cli
-yarn global add @warriorjs/cli
-
-yarn global add create-react-app
-yarn global add react-devtools
-yarn global add @react-native-community/cli
-yarn global add create-react-native-app
-yarn global add create-react-native-module
-
-# lint && babel plugins
-yarn global add babel-eslint
-yarn global add eslint
-yarn global add tslint
-yarn global add lint-staged
-yarn global add eslint-plugin-html
-yarn global add eslint-config-standard
-yarn global add eslint-plugin-node
-yarn global add eslint-plugin-promise
-yarn global add eslint-plugin-standard
+yarnGlobalAdd
